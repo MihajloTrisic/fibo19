2655fc1c918f997cbaad4ae153447b805e53d378
mtrisic <mtrisic@students.tgm.ac.at>
2016-03-31 09:36:45 +0200

    x1

--- Test3/Test/src/Test2.java
+++ Test3/Test/src/Test2.java
@@ -1,4 +1,13 @@
-
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+/*
+ * fibo19 git@github.com:MihajloTrisic/fibo19.git
+ *
+ */
 public class Test2 {
 
+	@Test
+	public void testFormel2(){
+		
+	}
 }
